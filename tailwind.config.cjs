35aac8bb4401540b349b0a3bc39b375e4bd253d5
Pham Anh Dung <anhdungpham090@gmail.com>
2022-08-14 23:28:29 +0700

    config eslint prettier.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		fontFamily: {
-			sans: ["Sora", "sans-serif"],
-			serif: ["Merriweather", "serif"],
+			sans: ['Sora', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
 		},
 		extend: {},
 	},
