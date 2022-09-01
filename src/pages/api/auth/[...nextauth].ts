64cfa047b2dd03aa8d0e58c1c0cc02614de99172
Pham Anh Dung <anhdungpham090@gmail.com>
2022-09-01 10:04:46 +0700

    config oauth.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '~/server/db/client';
 import { env } from '~/env/server.mjs';
-import { SIGNIN_PATH } from '~/data/constant';
 
 export const authOptions: NextAuthOptions = {
 	// Include user.id on session
@@ -14,6 +15,8 @@ export const authOptions: NextAuthOptions = {
 			if (session.user) {
 				session.user.id = user.id;
 			}
+			console.log({ session, user });
+
 			return session;
 		},
 	},
@@ -24,12 +27,16 @@ export const authOptions: NextAuthOptions = {
 			clientId: env.DISCORD_CLIENT_ID,
 			clientSecret: env.DISCORD_CLIENT_SECRET,
 		}),
+		GoogleProvider({
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		}),
+		GithubProvider({
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET,
+		}),
 		// ...add more providers here
 	],
-	pages: {
-		signIn: SIGNIN_PATH,
-		error: '/_error', // Error code passed in query string as ?error=
-	},
 };
 
 export default NextAuth(authOptions);
